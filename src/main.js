40171698384156147fe98afc6b7c32ebb0176e0c
cannobiscuit <542796517@qq.com>
2022-09-19 11:25:50 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant 核心组件库
+import Vant from 'vant'
+// 加载vant 全局样式
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+
+// 所有import都需要写在最前面
+
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
